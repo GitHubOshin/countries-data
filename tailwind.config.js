b9eb718999a9d98dfedb117b0d33e51f60a3ec30
OSHIN <izkp44@gmail.com>
2023-11-10 15:43:09 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    screens: { mobile: '375px', desktop: '1440px' },
+    fontFamily: { nunitoSans: ['Nunito Sans'] },
+    extend: {
+      colors: {
+        darkModeElements: 'hsl(209, 23%, 22%)',
+        darkModeBg: 'hsl(207, 26%, 17%)',
+        lightModeText: 'hsl(200, 15%, 8%)',
+        lightModeInput: 'hsl(0, 0%, 52%)',
+        lightModeBg: 'hsl(0, 0%, 98%)',
+        dmTextAndLmElements: 'hsl(0, 0%, 100%)'
+      }
+    }
   },
   plugins: []
 }
