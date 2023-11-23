87cabd1d765ac87ac9d78983403da6560c790317
GitHubOshin <izkp44@gmail.com>
2023-11-23 17:27:24 +0700

    update css

--- client/src/components/SearchCountry.tsx
+++ client/src/components/SearchCountry.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState, Dispatch, SetStateAction } from 'react'
+import { useContext, Dispatch, SetStateAction } from 'react'
 import { DarkModeContext } from '../App'
 import { TypeRegions } from '../interfaces/types'
 import { Listbox } from '@headlessui/react'
@@ -38,42 +38,39 @@ function SearchCountry(props: SearchCountryProps): JSX.Element {
     : 'bg-dmTextAndLmElements'
 
   function handleOnClickRegion(region: TypeRegions) {
-    // regions.map((region) => {
-    //   if (regionId === region.country) {
-    //     setActiveRegion('Filter by Region')
-    //     setDropdown(false)
-    //   } else if (regionId === region.id) {
-    //     setActiveRegion(region.country)
-    //     setDropdown(false)
-    //   }
-    // })
     setFilterRegion(region)
   }
 
   console.log(regions)
 
   return (
-    <section className="flex justify-between">
+    <section className=" flex justify-between">
       <input
-        className={`rounded px-[32px] py-4  desktop:min-w-[480px] ${isDark}`}
+        className={`rounded px-[32px] py-4   desktop:min-w-[480px] ${isDark}`}
         placeholder="Search for country..."
         value={searchCountry || ''}
         onChange={handleSetSearchCountry}
       />
 
-      <div>
+      <div className={`${isDark} w-[200px] rounded-md shadow-md`}>
         <Listbox value={filterRegion} onChange={handleOnClickRegion}>
-          <Listbox.Button className="w-[100px] text-left">
+          <Listbox.Button className="flex items-center justify-between  w-full h-full right-0 px-10">
             {filterRegion}
+            <img
+              alt="arrow down"
+              src="../../public/images/icons/arrowDown.svg"
+            />
           </Listbox.Button>
-          <Listbox.Options>
+          <Listbox.Options
+            className={`${isDark} py-3 rounded-md shadow-md absolute w-[200px] mt-1 `}
+          >
             {regions.map((region) => {
               return (
                 <Listbox.Option
                   key={region.value}
                   value={region.text}
                   className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    `relative cursor-default select-none py-2 pl-10 pr-4 hover:cursor-pointer ${
                       active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
                     }`
                   }
@@ -85,21 +82,6 @@ function SearchCountry(props: SearchCountryProps): JSX.Element {
           </Listbox.Options>
         </Listbox>
       </div>
-      {/* <ul
-          className={`absolute top-[60px] py-6 w-full rounded ${isDark} ${isDropdwn}`}
-        >
-          {regions.map((region) => {
-            return (
-              <li
-                onClick={() => handleOnClickRegion(region.text)}
-                key={region.value}
-                className="hover:bg-slate-300 hover:text-darkModeElements hover:cursor-pointer h-7 flex items-center pl-6"
-              >
-                {region.text}
-              </li>
-            )
-          })}
-        </ul> */}
     </section>
   )
 }
