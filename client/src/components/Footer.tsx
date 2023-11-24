2aed6f9c1b76fa4dac50e88426c0f29d88f0bacd
OSHIN <izkp44@gmail.com>
2023-11-25 03:18:55 +0700

    Update Footer.tsx

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,9 @@
 function Footer(): JSX.Element {
-  return <></>
+  return (
+    <section className="text-center m-16">
+      Created by OSHIN (Ganjanapas Phothong) & anonymous man
+    </section>
+  )
 }
 
 export default Footer
